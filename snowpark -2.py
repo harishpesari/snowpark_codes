6568702e29718b04fefb0cb7bbc2ffbdfe07bcf6
harishpesari <137114423+harishpesari@users.noreply.github.com>
2025-08-09 09:47:06 +0530

    Update snowpark -2.py

--- snowpark -2.py
+++ snowpark -2.py
@@ -4,8 +4,8 @@ from snowflake.snowpark.functions import *
 # Define connection parameters directly
 connection_parameters = {
     "account": "ISCJGIV-TX84484",
-    "user": "INDRANI",
-    "password": "LuchiBuchi@2610", 
+    "user": "****",
+    "password": "*****", 
     "role": "ACCOUNTADMIN",
     "warehouse": "SNOWFLAKE_LEARNING_WH",
     "database": "SNOWFLAKE_LEARNING_DB",
@@ -20,4 +20,5 @@ emp_df = session.table("EMPLOYEES")
 
 # Get employees who joined after 1st Jan 2021
 fil_df = emp_df.filter(col("DOJ") > to_date(lit("2021-01-01")))
+
 fil_df.show()
